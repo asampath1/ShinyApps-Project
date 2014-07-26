4a68b9b5cf3c8b0c5c3c438648648ec8e8fc9923
Anand Sampathkumaran <anand.sampathkumaran@gmail.com>
2014-07-26 15:12:41 +0530

    Update ui.R
    Added Water Consumption prediction and tab sets and write up which acts as a documentation for the project.

--- ui.R
+++ ui.R
@@ -5,37 +5,79 @@ shinyUI(pageWithSidebar(
         
         # Application title
         
-        headerPanel("A small application to see the summary & head/tail view of a selected data set"),
+        headerPanel("A small application"),
         
+        # Writeup tab acts as a documentation place holder which describes briefly about the project.
         # Sidebar with controls to provide a caption, select a dataset, and 
-        # specify the number of observations to view. Note that changes made
-        # to the caption in the textInput control are updated in the output
-        # area immediately as you type
+        # specify the number of observations to view. Select the head or tail set to be fetched.
+        # view the results in the Data Summary tab
+        # Select the temperature and the number of hours, 
+        # estimated water consumption will be displayed in Prediction tab
+        
         sidebarPanel(
-                textInput("caption", "Caption:", "Data Summary"),
-                
+                               
                 selectInput("dataset", "Choose a dataset:", 
                             choices = c("iris","mtcars")),
                 
                 selectInput("view", "Choose Head or Tail Rows:", 
                             choices = c("head","tail")),
                 
-                numericInput("obs", "Number of observations to view:", 10)
+                numericInput("obs", "Number of observations to view:", 10),
+                
+                br(),
+                
+                sliderInput("temperature", "Tempertature:", 
+                            min = -60, max = 120, value = 90, step= 10),
+                sliderInput("time", "Mowing Time(in hrs)", 
+                            min = 0.5, max = 5, value = 1.5, step= 0.5),
+                
+                submitButton('Submit')
                 
         ),
-        
-        
-        # Show the caption, a summary of the dataset and an HTML table with
-        # the requested number of observations
+
         mainPanel(
                 
-                h3(textOutput("caption")), 
-                
-                verbatimTextOutput("summary"),
-                
-                h2(textOutput("rows")),
-                
-                tableOutput("view")
+                tabsetPanel(
+                        tabPanel("Write Up", 
+                                 h3('Data Science Course Assignment- Develop Data Products'),
+                                 p('I have combined two different applications into one project as part of learning'),
+                                 h4('Data set Example'),
+                                 p('In this there are two sets mtcars and iris'),
+                                 p('use the left side panel to choose the dataset and the number of records to be fetched'),
+                                 p('Dataset Summary tab will display the results'),
+                                 h4('Prediction Example'),
+                                 p('Temperature, Mowing, Thirst Example'),
+                                 p('if the temperature were 90 degrees Fahrenheit and someone mowed for one and a half hours 
+                                 how much water would you estimate that they would want to drink during three hours outside?'),
+                                 p('Once we have the multiple regression equation, it is relatively straight forward to make predictions.
+                                 In this example, the regression equation was given by
+                                 Water = - 122 + 1.51*Temperature + 12.5*Mowing Time
+                                 Now set temperature to 90 degrees and mowing for 1.5 hours as given in the example. 
+                                 Water=-122+1.51*90+12.5*1.5=32.65 oz
+                                 We estimate that the person in the example would drink a little less than 33 ounces.'),
+                                 p('Please use the left side panel to choose the temperature and the number of hours of mowing time
+                                 Prediction tab will display the estimate that the person can consume water')
+                            ),
+                        tabPanel("Data Set Summary", 
+                                        h4('You have selected'),
+                                        h3(textOutput("dataset")),
+                                        h3(textOutput("obs")),
+                                        h3(textOutput("rows")),
+                                        tableOutput("view")
+                                 ),
+                        tabPanel("Prediction", 
+                                        h3('Results of Prediction'),
+                                        h4('You entered'),
+                                        verbatimTextOutput("temperature"),
+                                        verbatimTextOutput("time"),
+                                        h4('We estimate that the person in the example would drink a little less than'),
+                                        verbatimTextOutput("prediction"),
+                                        h4('Ounches')
+                        )
+             
+                                 
+                        )
+                )
+)
+)
 
-        )
-))
